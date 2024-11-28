cb4c79910ccd4f040cbdcb40832b94b7d4b2d59e
Siddhant Bhardwaj <bhardwajsiddhant03@gmail.com>
2024-11-28 10:33:04 +0530

    first commit

---
+++ app/src/main/java/com/siddhantbhardwaj/broadcastapp/AirplaneModeReceiver.java
@@ -0,0 +1,26 @@
+package com.siddhantbhardwaj.broadcastapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AirplaneModeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if(
+                intent.getAction() != null &&
+                intent.getAction().equals(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        ){
+            boolean isAirplaneMode = intent.getBooleanExtra("state",false);
+            String msg = isAirplaneMode ? "Airplane Mode is ON" : "Airplane Mode is OFF";
+            Toast.makeText(
+                    context,
+                    ""+msg,
+                    Toast.LENGTH_LONG
+            ).show();
+        }
+
+    }
+}
